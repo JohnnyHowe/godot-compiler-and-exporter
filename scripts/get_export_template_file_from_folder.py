f8f2d7427eeeeea9fba277857f553dc75a946f13
Johnny Howe <jonathon.o.howe@gmail.com>
2026-03-04 11:20:41 +1300

    Smarter picking export template file

--- scripts/get_export_template_file_from_folder.py
+++ scripts/get_export_template_file_from_folder.py
@@ -1,5 +1,6 @@
 """Helpers for locating platform-specific export templates in a folder."""
 from pathlib import Path
+from typing import Iterable
 
 
 class _ExportTemplateFinder:
@@ -12,20 +13,42 @@ class _ExportTemplateFinder:
 		"""Return the first matching template file for the platform."""
 		match platform.lower():
 			case "web":
-				return self._find_first_of_type(".zip")
+				return self._find_of_type(".zip")
 			case "windows":
-				return self._find_first_of_type(".exe")
+				return self._find_of_type(".exe")
 		print(
 			f"WARNING: No finder for platform {platform} implemented! Returning folder path...")
 		return self.folder
 
-	def _find_first_of_type(self, extension: str) -> Path:
+	def _find_of_type(self, extension: str) -> Path:
 		"""Find the first file in the folder with the given extension."""
+		options = list(self._find_all_of_type(extension))
+
+		# uh, oh
+		if len(options) == 0:
+			raise FileNotFoundError(
+				f"Could not find a {extension} file in {self.folder.resolve()}!")
+
+		# just one, very good
+		if len(options) == 1:
+			return options[0]
+
+		# console in one?
+		non_console_items = [path for path in options if not path.stem.endswith(".console")]
+		if len(non_console_items) == 1:
+			return non_console_items[0]
+
+		print("Could not decide what file export template is!")
+		print(f"\tFound {', '.join(options)}")
+		chosen = non_console_items[0]
+		print(f"\Taking first: {chosen}")
+		return chosen
+
+
+	def _find_all_of_type(self, extension: str) -> Iterable[Path]:
 		for item in self.folder.iterdir():
 			if item.suffix == extension:
-				return item
-		raise FileNotFoundError(
-			f"Could not find a {extension} file in {self.folder.resolve()}!")
+				yield item
 
 
 def get_export_template_file_from_folder(platform: str, folder: Path) -> Path:
